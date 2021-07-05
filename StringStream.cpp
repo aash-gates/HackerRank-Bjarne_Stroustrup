ccb00185749e34d237fd9345cb1de4e69a70186b
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-05 12:51:23 +0530

    Update StringStream.cpp

--- StringStream.cpp
+++ StringStream.cpp
@@ -2,26 +2,27 @@
 #include <vector>
 #include <iostream>
 using namespace std;
-
-vector<int> parseInts(string str) {
-string a;
-cin>>a;
-for (int i=0;i<a.size();i++)
+vector<int> parseInts(string str)
 {
-    if (a[i]!=',')
-        cout<<a[i];
-    else
-        cout<<"\n";    
+stringstream ss(str);
+vector<int> out;
+char ch;
+int temp;
+while(ss >> temp)
+{
+out.push_back(temp);
+ss >> ch;
 }
+return out;
 }
-
-int main() {
-    string str;
-    cin >> str;
-    vector<int> integers = parseInts(str);
-    for(int i = 0; i < integers.size(); i++) {
-        cout << integers[i] << "\n";
-    }
-    
-    return 0;
+int main()
+{
+string str;
+cin >> str;
+vector<int> integers = parseInts(str);
+for(int i = 0; i < integers.size(); i++)
+{
+cout << integers[i] << "\n";
+}
+return 0;
 }
