d0175715db6cbae08e9ac6fed1002c13a7d70b40
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-06 11:23:37 +0530

    Update Multi Level Inheritance.cpp

--- Multi Level Inheritance.cpp
+++ Multi Level Inheritance.cpp
@@ -1,44 +1,42 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-#include <memory> // std::unique_ptr, std::make_unique
-#include <array>  // std::array
+#include <algorithm>
+using namespace std;
 
-struct Shape
+class Triangle
 {
- // optional noexcept specifier
- virtual void triangle() const noexcept = 0;
- virtual ~Shape() = default;
+	public:
+		void triangle()
+        {
+			cout<<"I am a triangle\n";
+		}
 };
 
-struct Triangle : public Shape
+class Isosceles : public Triangle
 {
-  void triangle() const noexcept override {
-  std::cout << "I am a triangle\n";
- }
+  	public:
+  		void isosceles()
+        {
+    		cout<<"I am an isosceles triangle\n";
+  		}
 };
 
-struct Isosceles : public Triangle
+class Equilateral : public Isosceles 
 {
-  void triangle() const noexcept override {
-  std::cout << "I am an isosceles triangle\n";
- }
+    public:
+        void equilateral()
+        {
+            cout<<"I am an equilateral triangle\n";
+        }
 };
 
-struct Equilateral final : public Isosceles
-{
-  void triangle() const noexcept override {
-  std::cout << "I am an equilateral triangle\n";
- }
-};
-
-int main()
-{
- const std::array<std::unique_ptr<Shape>, 3> obj
- {
-	 std::make_unique<Equilateral>(), // element 1
-	 std::make_unique<Isosceles>(),   // element 2
-	 std::make_unique<Triangle>()	 // element 3
- };
-for (auto& it : obj)
-	it->triangle();
- return 0;
+int main(){
+  
+    Equilateral eqr;
+    eqr.equilateral();
+    eqr.isosceles();
+    eqr.triangle();
+    return 0;
 }
