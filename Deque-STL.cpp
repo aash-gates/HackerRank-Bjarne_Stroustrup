b1620d82302049d3ac185bc094f2cd87a4b5722b
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-08 09:26:06 +0530

    Update Deque-STL.cpp

--- Deque-STL.cpp
+++ Deque-STL.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
-#include <deque>
-
+#include <deque> 
 using namespace std;
 
-// A Dequeue (Double ended queue) based method for printing maixmum element of
-// all subarrays of size k
-void printKMax(int arr[], int n, int k)
-{
-    // Create a Double Ended Queue, Qi that will store indexes of array elements
-    // The queue will store indexes of useful elements in every window and it will
-    // maintain decreasing order of values from front to rear in Qi, i.e., 
-    // arr[Qi.front[]] to arr[Qi.rear()] are sorted in decreasing order
-    std::deque<int>  Qi(k);
+void printKMax(int arr[], int n, int k){
+	std::deque<int>  Qi(k);
 
     /* Process first k (or first window) elements of array */
     int i;
@@ -50,20 +42,19 @@ void printKMax(int arr[], int n, int k)
     cout << arr[Qi.front()] << endl;
 }
 
-// Driver program to test above functions
-int main()
-{
-    int t;
-    cin >> t;
-    while(t>0) {
-    int n,k;
-    cin >> n >> k;
-    int i;
-    int arr[n];
-    for(i=0;i<n;i++)
-    cin >> arr[i];
-    printKMax(arr, n, k);
-    t--;
-    }
-    return 0;
+int main(){
+  
+	int t;
+	cin >> t;
+	while(t>0) {
+		int n,k;
+    	cin >> n >> k;
+    	int i;
+    	int arr[n];
+    	for(i=0;i<n;i++)
+      		cin >> arr[i];
+    	printKMax(arr, n, k);
+    	t--;
+  	}
+  	return 0;
 }
