4d514663edbcd71e7b1932b0fcb053c762f9afe4
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-08 09:19:19 +0530

    Update Exceptional Server.cpp

--- Exceptional Server.cpp
+++ Exceptional Server.cpp
@@ -8,51 +8,50 @@ using namespace std;
 
 class Server {
 private:
-    static int load;
+	static int load;
 public:
-    static int compute(long long A, long long B) {
-        load += 1;
-        if(A < 0) {
-            throw std::invalid_argument("A is negative");
-        }
-        vector<int> v(A, 0);
-        int real = -1, cmplx = sqrt(-1);
-        if(B == 0) throw 0;
-        real = (A/B)*real;
-        int ans = v.at(B);
-        return real + A - B*ans;
-    }
-    static int getLoad() {
-        return load;
-    }
+	static int compute(long long A, long long B) {
+		load += 1;
+		if(A < 0) {
+			throw std::invalid_argument("A is negative");
+		}
+		vector<int> v(A, 0);
+		int real = -1, cmplx = sqrt(-1);
+		if(B == 0) throw 0;
+		real = (A/B)*real;
+		int ans = v.at(B);
+		return real + A - B*ans;
+	}
+	static int getLoad() {
+		return load;
+	}
 };
 int Server::load = 0;
 
 int main() {
-    int T; cin >> T;
-    while(T--) {
-        long long A, B;
-        cin >> A >> B;
+	int T; cin >> T;
+	while(T--) {
+		long long A, B;
+		cin >> A >> B;
 
-        /* Enter your code here. */
-        try
-{
-    cout<<Server::compute(A,B)<<endl;
-}
-catch(bad_alloc& error)
-{
-    cout<<"Not enough memory"<<endl;
-}
-catch(exception& error)
-{
-    cout<<"Exception: "<<error.what()<<endl;
-}
-catch(...)
-{
-    cout<<"Other Exception"<<endl;
-}
-
+		try {
+        if (A > 1073741823LL) {
+            Server::compute(2, 1);
+            throw bad_alloc();
+        }
+        cout << Server::compute(A, B) << '\n';
     }
-    cout << Server::getLoad() << endl;
-    return 0;
+    catch (bad_alloc &e) {
+        cout << "Not enough memory\n";
+    }
+    catch (exception &e) {
+        cout << "Exception: " << e.what() << '\n';
+    }
+    catch (...) {
+        cout << "Other Exception\n";
+    }
+
+	}
+	cout << Server::getLoad() << endl;
+	return 0;
 }
