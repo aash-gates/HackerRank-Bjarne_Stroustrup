e7722417bbf5be8c81d45caf0b233dd448ab4ee7
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-12 22:12:00 +0530

    Update Operator Overloading.cpp

---
+++ Operator Overloading.cpp
@@ -0,0 +1,53 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+//Overload operators + and << for the class complex
+//+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
+//<< should print a complex number in the format "a+ib"
+ostream& operator<<(ostream& os, const Complex& c) {
+    return os << c.a << (c.b > 0 ? '+' : '-') << 'i' << c.b;
+}
+
+Complex operator+(const Complex& a, const Complex& b) { 
+    return { a.a + b.a, a.b + b.b };
+}
+int main () {
+   int cases,k;
+   cin >> cases;
+   for(k=0;k<cases;k++) {
+      Matrix x;
+      Matrix y;
+      Matrix result;
+      int n,m,i,j;
+      cin >> n >> m;
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         x.a.push_back(b);
+      }
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         y.a.push_back(b);
+      }
+      result = x+y;
+      for(i=0;i<n;i++) {
+         for(j=0;j<m;j++) {
+            cout << result.a[i][j] << " ";
+         }
+         cout << endl;
+      }
+   }  
+   return 0;
+}
